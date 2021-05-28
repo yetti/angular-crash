17097310b29c4a02543972d9453075662bbe7747
Yetrina Battad <hello@yetti.io>
2021-05-29 08:46:41 +1000

    feat: complete tutorial

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TaskService } from '../../services/task.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((task) => this.tasks = task);
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    console.log(task);
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+}
